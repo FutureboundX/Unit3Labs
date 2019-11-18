f5537862608fb04a6510db16297e9961e14d6096
FutureboundX <48761556+FutureboundX@users.noreply.github.com>
2019-11-18 08:32:35 -0500

    Create problem1.java

---
+++ problem1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class problemOne {
+	public static void main(String[] args) {
+		//Gets first word from user
+		System.out.println("Enter your first word: ");
+		Scanner input = new Scanner(System.in);
+		String wordOne = new String(input.nextLine( ));
+		
+		//Gets second word from user
+		System.out.println("Enter your second word: ");
+		input = new Scanner(System.in);
+		String wordTwo = new String(input.nextLine( ));
+		
+		
+	}
+
+}
